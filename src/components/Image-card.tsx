4bf0816525b55053b249456010c43d6f17794a1a
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-03-27 00:22:06 +0900

    card list hover & checkbox

--- src/components/Image-card.tsx
+++ src/components/Image-card.tsx
@@ -1,27 +1,30 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "../scss/imagecard.scss";
 import { Link } from 'react-router-dom';
 import { Data } from '../misc/model';
 
-
 function ImageCard({...data}:Data[]) {
     const imageArr = Object.values(data);
-    console.log(imageArr[0]);
+    const [checked, setChecked] =useState<number[]>([])
+    const inputRef = useRef<HTMLInputElement>(null)
+    const onChecked = (e:React.ChangeEvent<HTMLInputElement>)=>{
+        setChecked(i=>[...i, parseInt(e.target.id)]);
+    }
+  
   return (
   <>
         {imageArr.map((i, idx)=>
             <li key={idx} className='card-item' >
                 <div className='card-pic' >
                     <img src={i._id}/>
-                <span className='card-view'>일인칭 뷰어</span>
+                    <span className='card-view'>일인칭 뷰어</span>
                 </div>
                 <div className='card-hover-container'>
-                    <input id="select" type="checkbox"/>
-                <Link to={{pathname:`/image/${idx}`, search:`${idx}`}}>
-                    <div className="card-hover">
-                        <button className='card-option' type='button'>...</button>
-                    </div>
-                </Link>
+                    <Link to={{pathname:`/image/${idx}`, search:`${idx}`}}>
+                        <div className="card-hover"></div>
+                    </Link>
+                    <input className='checkbox' type="checkbox" onChange={onChecked} id={`${idx}`}/>
+                    <button className='card-option' type='button'>...</button>
                 </div>
             </li>
         )}
